04b6a59328f9264fe60e9e16e18cff88c5766492
Arsen Musayelyan <arsen@arsenm.dev>
2022-10-01 01:32:48 -0700

    Add itd-git package

---
+++ itd-git/lure.sh
@@ -0,0 +1,33 @@
+name='itd-git'
+version='r188.271510d'
+release=1
+desc='Companion daemon for the InfiniTime firmware on the PineTime smartwatch'
+homepage='https://gitea.arsenm.dev/Arsen6331/itd'
+architectures=('amd64' 'aarch64' 'arm7' 'arm6' '386')
+license=('GPLv3')
+provides=('itd' 'itctl')
+conflicts=('itd' 'itctl')
+
+build_deps=('golang')
+build_deps_arch=('go')
+build_deps_alpine=('go')
+build_deps_opensuse=('go')
+
+deps=('dbus' 'bluez' 'pulseaudio-utils')
+deps_arch=('dbus' 'bluez' 'libpulse')
+deps_opensuse=('dbus-1' 'bluez' 'pulseaudio-utils')
+
+sources=("git+https://gitea.arsenm.dev/Arsen6331/itd.git")
+checksums=('SKIP')
+
+backup=('/etc/itd.toml')
+
+build() {
+	cd "$srcdir/itd"
+	make
+}
+
+package() {
+	cd "$srcdir/itd"
+	make DESTDIR="$pkgdir" PREFIX="/usr" install
+}
