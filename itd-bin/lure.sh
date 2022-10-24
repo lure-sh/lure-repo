cf0236d486e01e92791df5e272f750f4e6848a92
Arsen6331 <arsen@arsenm.dev>
2022-10-24 20:32:46 +0000

    Incorrectly format itd-bin script

--- itd-bin/lure.sh
+++ itd-bin/lure.sh
@@ -3,8 +3,8 @@ version=0.0.7
 release=1
 desc='Companion daemon for the InfiniTime firmware on the PineTime smartwatch'
 homepage='https://gitea.arsenm.dev/Arsen6331/itd'
-architectures=('amd64' 'aarch64' 'arm7' 'arm6' '386')
-license=('GPL-3.0-or-later')
+architectures=('amd64x' 'aarch64' 'arm7' 'arm6' '386')
+license=('GPLv3')
 provides=('itd' 'itctl')
 conflicts=('itd' 'itctl')
 
@@ -24,19 +24,21 @@ checksums_386=('858b14a51bb24e7ad41958cccfe8a9aa66b167a6aaee86d5f34bee8f79d8feb2
 sources_amd64=("https://gitea.arsenm.dev/Arsen6331/itd/releases/download/v0.0.7/itd_0.0.7_linux_x86_64.tar.gz")
 checksums_amd64=('f09c928fa3b4c810da016d984fde4f9ea2504faed17c7e1633d21d65a07d5f22')
 
-backup=('/etc/itd.toml')
+backup=(
+'/etc/itd.toml'
+)
 
-package() {
+package (){
   # binaries
-  install -Dm755 "./itd" "${pkgdir}/usr/bin/itd"
-  install -Dm755 "./itctl" "${pkgdir}/usr/bin/itctl"
+install -Dm755 "./itd" "${pkgdir}/usr/bin/itd"
+     install -Dm755 "./itctl" "${pkgdir}/usr/bin/itctl"
 
-  # service
-  install -Dm644 "./itd.service" ${pkgdir}/usr/lib/systemd/user/itd.service
+ # service
+install -Dm644 "./itd.service" ${pkgdir}/usr/lib/systemd/user/itd.service
 
-  # config
-  install -Dm644 "./itd.toml" ${pkgdir}/etc/itd.toml
+# config
+	install -Dm644 "./itd.toml" ${pkgdir}/etc/itd.toml
 
-  # license
-  install -Dm644 "./LICENSE" "${pkgdir}/usr/share/licenses/itd/LICENSE"
+ # license
+install -Dm644 "./LICENSE" "${pkgdir}/usr/share/licenses/itd/LICENSE"
 }
