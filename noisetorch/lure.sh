9fe86712b4d7b114ea344ef65d2dfab3244a54a7
e.khramov <e.khramov@uecrus.com>
2022-10-26 12:14:31 +0300

    add NoiseTorch

--- noisetorch/lure.sh
+++ noisetorch/lure.sh
@@ -11,38 +11,16 @@ conflicts=('noisetorch')
 deps_amd64=('git go')
 build_deps=('git' 'go')
 
-sources_amd64=("git+https://github.com/noisetorch/NoiseTorch.git#tag=v${version}"
-	#"git+https://github.com/noisetorch/c-ringbuf.git"
-	#"git+https://github.com/noisetorch/rnnoise.git"
-)
-checksums_amd64=('SKIP'
-	#'SKIP'
-	#'SKIP'
-)
+sources_amd64=("git+https://github.com/noisetorch/NoiseTorch.git#tag=v${version}")
+checksums_amd64=('SKIP')
 
 prepare() {
 	cd "${srcdir}/NoiseTorch"
-	#git submodule init
-	#git config submodule."c/c-ringbuf.url" "${srcdir}/c-ringbuf"
-	#git config submodule."c/rnnoise.url" "${srcdir}/rnnoise"
-	#git submodule update
 }
 
 build() {
 	cd "${srcdir}/NoiseTorch"
-	#export GOPATH="${srcdir}/go"
-	#pushd "c/ladspa"
 	make
-	#popd
-	#vendor_flags="-X main.version=${version}"
-	#export CGO_CPPFLAGS="${CPPFLAGS}"
-	#export CGO_CFLAGS="${CFLAGS}"
-	#export CGO_CXXFLAGS="${CXXFLAGS}"
-	#export CGO_LDFLAGS="${LDFLAGS}"
-	#export GOFLAGS="-buildmode=pie -trimpath -mod=readonly -modcacherw"
-	#go generate
-	#go build -ldflags "${vendor_flags} -linkmode=external" -o bin/noisetorch
-	#go clean -modcache
 }
 
 package() {
