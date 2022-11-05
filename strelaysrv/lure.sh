32fddaabf299589aead3143e7b6ec84d8268e445
xpamych <xpamych@yandex.ru>
2022-11-05 23:41:48 +0300

    add deb deps

--- strelaysrv/lure.sh
+++ strelaysrv/lure.sh
@@ -3,28 +3,30 @@ version='1.22.1'
 release='1'
 desc='This is the relay server for the syncthing project.'
 homepage='https://syncthing.net/'
-architectures=('amd64')
-licenses=('MPL-2.0')
-provides=('syncthing-relaysrv')
-conflicts=('syncthing-relaysrv')
-deps_amd64=('glibc')
+architectures='amd64'
+licenses='MPL'
+provides='syncthing-relaysrv'
+conflicts='syncthing-relaysrv'
+deps_amd64=('libc6')
 build_deps=('git' 'go')
-sources=("https://github.com/syncthing/relaysrv/releases/download/v${version}/${name}-linux-amd64-v${version}.tar.gz"
+build_deps_debian=('git' 'golang')
+
+sources_amd64=("https://github.com/syncthing/relaysrv/releases/download/v${version}/${name}-linux-amd64-v${version}.tar.gz"
 	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.service"
 	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.tmpfiles"
-	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.sysusers")
+"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.sysusers")
 checksums=('04028d3eb50bc6f007da1b65c2b37f9b8b3109efe4c8973f285893872081959b'
-	'SKIP'
-	'SKIP'
-	'SKIP')
+'SKIP'
+'SKIP'
+'SKIP')
 
 prepare() {
-	cd "${srcdir}"
+cd "${srcdir}"
 }
 
 package() {
-	install -Dm755 ./${name}-linux-amd64-v${version}/strelaysrv "${pkgdir}/usr/bin/syncthing-relaysrv"
-	install -Dm644 "${srcdir}/syncthing-relaysrv.service" "${pkgdir}/usr/lib/systemd/system/syncthing-relaysrv.service"
-	install -Dm644 "${srcdir}/syncthing-relaysrv.tmpfiles" "${pkgdir}/usr/lib/tmpfiles.d/syncthing-relaysrv.conf"
-	install -Dm644 "${srcdir}/syncthing-relaysrv.sysusers" "${pkgdir}/usr/lib/sysusers.d/syncthing-relaysrv.conf"
+install -Dm755 ./${name}-linux-amd64-v${version}/strelaysrv "${pkgdir}/usr/bin/syncthing-relaysrv"
+install -Dm644 "${srcdir}/syncthing-relaysrv.service" "${pkgdir}/usr/lib/systemd/system/syncthing-relaysrv.service"
+install -Dm644 "${srcdir}/syncthing-relaysrv.tmpfiles" "${pkgdir}/usr/lib/tmpfiles.d/syncthing-relaysrv.conf"
+install -Dm644 "${srcdir}/syncthing-relaysrv.sysusers" "${pkgdir}/usr/lib/sysusers.d/syncthing-relaysrv.conf"
 }
--- syncthing/lure.sh
+++ syncthing/lure.sh
@@ -3,14 +3,16 @@ version='1.22.1'
 release='1'
 desc='Syncthing is a continuous file synchronization program. It synchronizes files between two or more computers.'
 homepage='https://syncthing.net/'
-architectures=('amd64')
-licenses=('MPL-2.0')
-provides=('syncthing')
-conflicts=('syncthing')
-deps_amd64=('glibc' 'strelaysrv')
+architectures='amd64'
+licenses='MPL'
+provides='syncthing'
+conflicts='syncthing'
+deps_amd64=('libc6' 'strelaysrv')
 build_deps=('git' 'go')
+build_deps_debian=('git' 'golang')
+
 sources_amd64=("https://github.com/${name}/${name}/releases/download/v${version}/${name}-linux-amd64-v${version}.tar.gz")
-checksums_amd64=('f91066b8588c02b5ff77aa801cc4e0319f3467325bdd75326aebccffb6495698')
+checksums=('f91066b8588c02b5ff77aa801cc4e0319f3467325bdd75326aebccffb6495698')
 
 prepare() {
 	cd "${srcdir}"
@@ -18,9 +20,9 @@ prepare() {
 
 package() {
 	install -Dm755 ./${name}-linux-amd64-v${version}/syncthing "${pkgdir}/usr/bin/syncthing"
-	#install -Dm644 man/strelaysrv.1 "${pkgdir}/usr/share/man/man1/${name}.1"
-	install -Dm644 ./${name}-linux-amd64-v${version}/README.txt "${pkgdir}/usr/share/doc/syncthing/README.md"
-	install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/system/syncthing-resume.service" "${pkgdir}/usr/lib/systemd/system/syncthing-resume.service"
-	install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/system/syncthing@.service" "${pkgdir}/usr/lib/systemd/system/syncthing@.service"
-	install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/user/syncthing.service" "${pkgdir}/usr/lib/systemd/user/syncthing.service"
+#install -Dm644 man/strelaysrv.1 "${pkgdir}/usr/share/man/man1/${name}.1"
+install -Dm644 ./${name}-linux-amd64-v${version}/README.txt "${pkgdir}/usr/share/doc/syncthing/README.md"
+install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/system/syncthing-resume.service" "${pkgdir}/usr/lib/systemd/system/syncthing-resume.service"
+install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/system/syncthing@.service" "${pkgdir}/usr/lib/systemd/system/syncthing@.service"
+install -Dm644 "./${name}-linux-amd64-v${version}/etc/linux-systemd/user/syncthing.service" "${pkgdir}/usr/lib/systemd/user/syncthing.service"
 }
