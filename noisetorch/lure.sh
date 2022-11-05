242f08967f4c319878dc91810cda02416a0600be
xpamych <xpamych@yandex.ru>
2022-11-05 17:04:23 +0300

    add deb deps

--- noisetorch/lure.sh
+++ noisetorch/lure.sh
@@ -9,10 +9,12 @@ provides=('noisetorch')
 conflicts=('noisetorch')
 
 deps_amd64=('git' 'go')
+deps_amd64_debian=('git' 'golang')
 build_deps=('git' 'go')
+build_deps_debian=('git' 'golang')
 
 sources_amd64=("git+https://github.com/noisetorch/NoiseTorch.git#tag=v${version}")
-checksums_amd64=('SKIP')
+checksums_amd64=('4a30650314a6f83d979f3303d49618847ba58e39e3e26482dfe58b5e261c5611')
 
 prepare() {
 	cd "${srcdir}/NoiseTorch"
@@ -24,7 +26,7 @@ build() {
 }
 
 package() {
-	install -Dm755 "${srcdir}/NoiseTorch/bin/noisetorch" "${pkgdir}/usr/bin/noisetorch"
-	install -Dm644 "${srcdir}/NoiseTorch/assets/noisetorch.desktop" "${pkgdir}/usr/share/applications/noisetorch.desktop"
-	install -Dm644 "${srcdir}/NoiseTorch/assets/icon/noisetorch.png" "${pkgdir}/usr/share/icons/hicolor/256x256/apps/noisetorch.png"
+install -Dm755 "${srcdir}/NoiseTorch/bin/noisetorch" "${pkgdir}/usr/bin/noisetorch"
+install -Dm644 "${srcdir}/NoiseTorch/assets/noisetorch.desktop" "${pkgdir}/usr/share/applications/noisetorch.desktop"
+install -Dm644 "${srcdir}/NoiseTorch/assets/icon/noisetorch.png" "${pkgdir}/usr/share/icons/hicolor/256x256/apps/noisetorch.png"
 }
