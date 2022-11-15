735859e9b2bee5147cd8ea1cbe46dca9adae31b7
iamwacko <iamawacko@protonmail.com>
2022-11-14 19:16:41 -0800

    chore: change synth and oniongen-rs to use rustup in lure

--- synth/lure.sh
+++ synth/lure.sh
@@ -1,6 +1,6 @@
 name='synth'
 version='0.6.8'
-release='1'
+release='2'
 desc='The Open Source Data Generator'
 homepage='https://getsynth.com/'
 maintainer='s e <iamawacko@protonmail.com>'
@@ -11,32 +11,13 @@ checksums='18996ebb6a7a8a9d36d961fd7fc1dee38d78b0d03c57ab5681f312600ab5139e'
 provides=('synth')
 conflicts=('synth')
 
-build_deps=('wget' 'libsqlite3-dev')
+build_deps=('rustup' 'libsqlite3-dev')
 build_deps_arch=('rustup' 'sqlite')
 build_deps_opensuse=('rustup' 'sqlite3')
 build_deps_alpine=('rustup' 'sqlite-dev')
-build_deps_fedora=('wget' 'libsq3-devel')
-
-prepare_arch() {
-	rustup install nightly
-	cd "${srcdir}/${name}-${version}"
-	cargo fetch --locked
-}
-
-prepare_opensuse() {
-	rustup install nightly
-	cd "${srcdir}/${name}-${version}"
-	cargo fetch --locked
-}
-
-prepare_alpine() {
-	rustup install nightly
-	cd "${srcdir}/${name}-${version}"
-	cargo fetch --locked
-}
+build_deps_fedora=('rustup' 'libsq3-devel')
 
 prepare() {
-	rustup --version || wget https://sh.rustup.rs -O rustup.sh && sh rustup.sh -y 
 	rustup install nightly
 	cd "${srcdir}/${name}-${version}"
 	cargo fetch --locked
