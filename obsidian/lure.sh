94a8d0217281a47b3104bfe7cc0e8a1f75786813
xpamych <xpamych@yandex.ru>
2022-12-02 20:38:00 +0300

    add obsidian.sh & obsidian.desktop files (#25)

--- obsidian/lure.sh
+++ obsidian/lure.sh
@@ -22,26 +22,14 @@ prepare() {
 
 	cd "${srcdir}"
 	~/.npm/node_modules/asar/bin/asar.js ef "${name}-${version}/resources/obsidian.asar" icon.png
-	echo "[Desktop Entry]" >>obsidian.desktop
-	echo "Name=Obsidian" >>obsidian.desktop
-	echo "Exec=/usr/bin/obsidian %U" >>obsidian.desktop
-	echo "Terminal=false" >>obsidian.desktop
-	echo "Type=Application" >>obsidian.desktop
-	echo "Icon=obsidian" >>obsidian.desktop
-	echo "StartupWMClass=obsidian" >>obsidian.desktop
-	echo "Comment=Obsidian" >>obsidian.desktop
-	echo "MimeType=x-scheme-handler/obsidian;" >>obsidian.desktop
-	echo "Categories=Office;" >>obsidian.desktop
-	echo "#!/bin/sh" >>"${name}"
-	echo "exec ~/.npm/node_modules/electron/dist/electron /usr/lib/obsidian/app.asar \"\$@\"" >>"${name}"
 }
 
 package() {
 	# binaries
-	install -Dm755 "${name}" "${pkgdir}/usr/bin/obsidian"
+install -Dm755 "${repodir}/${name}" "${pkgdir}/usr/bin/obsidian"
 
 	# desctop file
-	install -Dm644 "${name}.desktop" -t "${pkgdir}/usr/share/applications/"
+	install -Dm644 "${repodir}/${name}.desktop" -t "${pkgdir}/usr/share/applications/"
 	install -Dm644 ./icon.png "${pkgdir}/usr/share/pixmaps/obsidian.png"
 	cd "${srcdir}/${name}-${version}/resources/"
 	install -dm0755 "$pkgdir/usr/lib/obsidian"
