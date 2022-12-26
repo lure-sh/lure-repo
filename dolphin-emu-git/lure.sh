b001de36e4c1d40e28e2504b58b648f100e74827
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-12-26 21:39:57 +0700

    Add more build deps

--- dolphin-emu-git/lure.sh
+++ dolphin-emu-git/lure.sh
@@ -3,7 +3,7 @@ version='38515.f4b1155b4d'
 release=1
 architectures=('amd64')
 
-build_deps=('g++')
+build_deps=('g++' 'pkg-config' 'libudev-dev' 'libevdev-dev' 'qtbase5-dev' 'qtbase5-private-dev' 'libxi-dev')
 
 sources=('git+https://github.com/dolphin-emu/dolphin.git')
 checksums=('SKIP')
