a0d8d37e23c2c9d91e5fd40a82ccc727c31169ae
xpamych <xpamych@yandex.ru>
2023-01-15 13:18:08 +0300

    reset commits

--- obsidian-bin/lure.sh
+++ obsidian-bin/lure.sh
@@ -13,7 +13,7 @@ deps=('zlib' 'hicolor-icon-theme' 'fuse3')
 deps_debian=('zlib1g' 'hicolor-icon-theme' 'fuse3')
 build_deps=('npm')
 
-sources_amd64=("https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/${provides}-${version}.tar.gz")
+sources_amd64=("https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/obsidian-${version}.tar.gz")
 checksums_amd64=('SKIP')
 
 prepare() {
@@ -23,15 +23,13 @@ prepare() {
 	npm install electron --save-dev
 
 	cd "${srcdir}"
-	~/.npm/node_modules/asar/bin/asar.js ef "${provides}-${version}/resources/obsidian.asar" icon.png
+	~/.npm/node_modules/asar/bin/asar.js ef "obsidian-${version}/resources/obsidian.asar" icon.png
 }
 
 package() {
-	install -Dm755 "${scriptdir}/${provides}" "${pkgdir}/usr/bin/obsidian"
-	install -Dm644 "${scriptdir}/${provides}.desktop" -t "${pkgdir}/usr/share/applications/"
+	install -Dm755 "${scriptdir}/obsidian" "${pkgdir}/usr/bin/obsidian"
+	install -Dm644 "${scriptdir}/obsidian.desktop" -t "${pkgdir}/usr/share/applications/"
 	install -Dm644 ./icon.png "${pkgdir}/usr/share/pixmaps/obsidian.png"
-	cd "${srcdir}/${provides}-${version}/resources/"
-	install -dm0755 "$pkgdir/usr/lib/obsidian"
-	find . -type d -exec install -d {,"${pkgdir}/usr/lib/obsidian/"}{} \;
-	find . -type f -exec install -D {,"${pkgdir}/usr/lib/obsidian/"}{} \;
+    mkdir -p "${pkgdir}/usr/lib/obsidian"
+    cp -a "${srcdir}/obsidian-${version}/resources/." "${pkgdir}/usr/lib/obsidian"
 }
--- syncthing-relaysrv/lure.sh
+++ syncthing-relaysrv/lure.sh
@@ -1,4 +1,4 @@
-name='strelaysrv-bin'
+name='syncthing-relaysrv'
 version='1.22.1'
 release='1'
 desc='This is the relay server for the syncthing project.'
@@ -12,7 +12,7 @@ deps=('glibc')
 deps_debian=('libc6')
 build_deps=('git' 'go')
 
-sources_amd64=("https://github.com/syncthing/relaysrv/releases/download/v${version}/${provides}-linux-amd64-v${version}.tar.gz"
+sources=("https://github.com/syncthing/relaysrv/releases/download/v${version}/strelaysrv-linux-amd64-v${version}.tar.gz"
 	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.service"
 	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.tmpfiles"
 	"https://raw.githubusercontent.com/archlinux/svntogit-community/packages/syncthing/trunk/syncthing-relaysrv.sysusers")
@@ -26,7 +26,7 @@ prepare() {
 }
 
 package() {
-	install -Dm755 ./${provides}-linux-amd64-v${version}/strelaysrv "${pkgdir}/usr/bin/syncthing-relaysrv"
+	install -Dm755 "./strelaysrv-linux-amd64-v${version}/strelaysrv" "${pkgdir}/usr/bin/syncthing-relaysrv"
 	install -Dm644 "${srcdir}/syncthing-relaysrv.service" "${pkgdir}/usr/lib/systemd/system/syncthing-relaysrv.service"
 	install -Dm644 "${srcdir}/syncthing-relaysrv.tmpfiles" "${pkgdir}/usr/lib/tmpfiles.d/syncthing-relaysrv.conf"
 	install -Dm644 "${srcdir}/syncthing-relaysrv.sysusers" "${pkgdir}/usr/lib/sysusers.d/syncthing-relaysrv.conf"
