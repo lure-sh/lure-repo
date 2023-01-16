3ce01c044cb68aebc84426a3077178c4c91afb4c
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-01-16 18:28:27 +0300

    upg(img2pdf+): v1.1.3 (#37)

--- img2pdf+/lure.sh
+++ img2pdf+/lure.sh
@@ -1,33 +1,34 @@
 name="img2pdf+"
-version=1.1
-release=1
-desc="Merge images into one pdf file via command line. "
-homepage="https://github.com/Elagoht/img2pdf"
+version=1.1.1
+release=2
+desc="Merge images into one pdf file via command line."
+homepage="https://github.com/Elagoht/img2pdf-plus"
 architectures=("all")
-license=("GPL-3.0-or-later")
+license=("MIT")
 maintainer="Furkan Baytekin (Elagoht) <furkanbaytekin@gmail.com>"
 provides=("img2pdf+")
 build_deps=("python3" "python3-pip")
 build_deps_arch=("python" "python-pip")
 build_deps_opensuse=("python3" "python-pip")
-sources=("git+https://github.com/Elagoht/img2pdf")
+sources=("git+https://github.com/Elagoht/img2pdf-plus")
 checksums=("SKIP")
 version() {
-	cd "$srcdir/img2pdf"
+	cd "$srcdir/img2pdf-plus/src/img2pdf"
 	printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
 }
 prepare() {
-	cd "$srcdir/img2pdf/"
+	cd "$srcdir/img2pdf-plus/"
 	python3 -m venv builder
 	source builder/bin/activate
 	pip install pillow python-magic fpdf2 fonttools pyinstaller
 }
 build() {
-	cd "$srcdir/img2pdf/"
-	python -m PyInstaller --onefile img2pdf.py
+	cd "$srcdir/img2pdf-plus/"
+	$srcdir/img2pdf-plus/builder/bin/pyinstaller --onefile src/img2pdf/main.py --name=img2pdf+
 }
 package() {
 	install -d "$pkgdir/usr/bin/"
-	install -Dm755 "$srcdir/img2pdf/dist/img2pdf" -t "$pkgdir/usr/bin/"
-	rm -rf "$srcdir/img2pdf"
+	install -Dm755 "$srcdir/img2pdf-plus/dist/img2pdf+" -t "$pkgdir/usr/bin/"
+	#rm -rf "$srcdir/img2pdf-plus"
 }
+
