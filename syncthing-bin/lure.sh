7e71f8657a01c00d19290a8cc1b946c509ce0f83
xpamych <xpamych@yandex.ru>
2023-02-08 11:20:35 +0300

    upd(syncthing) 1.23.1

--- syncthing-bin/lure.sh
+++ syncthing-bin/lure.sh
@@ -1,5 +1,5 @@
 name='syncthing-bin'
-version='1.23.0'
+version='1.23.1'
 release='1'
 desc='Syncthing is a continuous file synchronization program. It synchronizes files between two or more computers.'
 homepage='https://syncthing.net/'
