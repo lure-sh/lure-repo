c5d233f5cdc2aaf73d4b522b77c26e4d10056f49
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-03-09 22:54:03 +0300

    upd(img2pdf+): 1.1.3-3 (#40)

--- img2pdf+/lure.sh
+++ img2pdf+/lure.sh
@@ -1,20 +1,20 @@
 name="img2pdf+"
-version=1.1.1
-release=2
+version=1.1.3
+release=3
 desc="Merge images into one pdf file via command line."
 homepage="https://github.com/Elagoht/img2pdf-plus"
 architectures=("all")
 license=("MIT")
 maintainer="Furkan Baytekin (Elagoht) <furkanbaytekin@gmail.com>"
 provides=("img2pdf+")
-build_deps=("python3" "python3-pip")
-build_deps_arch=("python" "python-pip")
-build_deps_opensuse=("python3" "python-pip")
+build_deps=("python3" "python3-pip" "python3-venv")
+build_deps_debian=("python3" "python3-venv" "python3-pip")
+build_deps_arch=("python" "python-pip" "python-virtualenv")
+build_deps_opensuse=("python3" "python-pip" "python-virtualenv")
 sources=("git+https://github.com/Elagoht/img2pdf-plus")
 checksums=("SKIP")
 version() {
-	cd "$srcdir/img2pdf-plus/src/img2pdf"
-	printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+	printf "%s" $version
 }
 prepare() {
 	cd "$srcdir/img2pdf-plus/"
