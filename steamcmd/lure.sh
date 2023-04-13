ef1aa73142affcd3bf608b42781c2a79b05c7a7b
xpamych <xpamych@yandex.ru>
2023-04-13 14:18:15 +0300

    fix(steamcmd)

--- steamcmd/lure.sh
+++ steamcmd/lure.sh
@@ -12,8 +12,7 @@ deps_amd64=('libgcc(x86-32)')
 deps_amd64_arch=('lib32-gcc-libs')
 deps_amd64_rosa=('lib64gcc1')
 
-
-    sources=("https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz")
+sources=("https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz")
 checksums=('SKIP')
 
 prepare() {
@@ -23,8 +22,8 @@ prepare() {
 package() {
 	install -Dm755 "${scriptdir}/steamcmd" "${pkgdir}/usr/bin/steamcmd"
 	install -Dm755 "${srcdir}/steamcmd.sh" "${pkgdir}/usr/share/steamcmd/steamcmd.sh"
-    install -Dm755 "${srcdir}/linux32/crashhandler.so" "${pkgdir}/usr/share/steamcmd/linux32/crashhandler.so"
-    install -Dm755 "${srcdir}/linux32/libstdc++.so.6" "${pkgdir}/usr/share/steamcmd/linux32/libstdc++.so.6"
-    install -Dm755 "${srcdir}/linux32/steamcmd" "${pkgdir}/usr/share/steamcmd/linux32/steamcmd"
-    install -Dm755 "${srcdir}/linux32/steamerrorreporter" "${pkgdir}/usr/share/steamcmd/linux32/steamerrorreporter"
+	install -Dm755 "${srcdir}/linux32/crashhandler.so" "${pkgdir}/usr/share/steamcmd/linux32/crashhandler.so"
+	install -Dm755 "${srcdir}/linux32/libstdc++.so.6" "${pkgdir}/usr/share/steamcmd/linux32/libstdc++.so.6"
+	install -Dm755 "${srcdir}/linux32/steamcmd" "${pkgdir}/usr/share/steamcmd/linux32/steamcmd"
+	install -Dm755 "${srcdir}/linux32/steamerrorreporter" "${pkgdir}/usr/share/steamcmd/linux32/steamerrorreporter"
 }
