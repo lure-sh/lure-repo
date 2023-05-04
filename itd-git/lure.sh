fb4c30dbb6876ae60fa85da308cfee6a7f27edfa
Elara Musayelyan <elara@elara.ws>
2023-05-04 03:32:45 +0000

    fix(itd-git): update domain

--- itd-git/lure.sh
+++ itd-git/lure.sh
@@ -3,9 +3,9 @@ version='188.271510d'
 release=1
 desc='Companion daemon for the InfiniTime firmware on the PineTime smartwatch'
 desc_ru='Демон-компаньон для прошивки InfiniTime на смарт-часах PineTime'
-maintainer='Arsen Musayelyan <arsen@arsenm.dev>'
-maintainer_ru='Арсен Мусаелян <arsen@arsenm.dev>'
-homepage='https://gitea.arsenm.dev/Arsen6331/itd'
+maintainer='Elara Musayelyan <elara@elara.ws>'
+maintainer_ru='Элара Мусаелян <elara@elara.ws>'
+homepage='https://gitea.elara.ws/Elara6331/itd'
 architectures=('amd64' 'arm64' 'arm7' 'arm6' '386')
 license=('GPL-3.0-or-later')
 provides=('itd' 'itctl')
@@ -20,7 +20,7 @@ deps=('dbus' 'bluez' 'pulseaudio-utils')
 deps_arch=('dbus' 'bluez' 'libpulse')
 deps_opensuse=('dbus-1' 'bluez' 'pulseaudio-utils')
 
-sources=("git+https://gitea.arsenm.dev/Arsen6331/itd.git")
+sources=("git+https://gitea.elara.ws/Elara6331/itd.git")
 checksums=('SKIP')
 
 backup=('/etc/itd.toml')
