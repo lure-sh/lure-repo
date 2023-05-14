c1e237afe601f84ccf703a00f7b0b0c4f67283f7
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-05-14 21:46:12 +0700

    Add build deps for Ubuntu

--- z88dk-git/lure.sh
+++ z88dk-git/lure.sh
@@ -6,6 +6,8 @@ architectures=('amd64')
 sources=('git+https://github.com/z88dk/z88dk?~recursive=true')
 checksums=('SKIP')
 
+build_deps_ubuntu=('build-essential' 'bison' 'flex' 'libxml2-dev' 'subversion' 'zlib1g-dev' 'm4' 'ragel' 're2c' 'dos2unix' 'texinfo' 'texi2html' 'gdb' 'curl' 'perl' 'cpanminus' 'ccache' 'libboost-all-dev' 'libmodern-perl-perl' 'libyaml-perl' 'liblocal-lib-perl' 'libcapture-tiny-perl' 'libpath-tiny-perl' 'libtext-table-perl' 'libdata-hexdump-perl' 'libregexp-common-perl' 'libclone-perl' 'libfile-slurp-perl')
+
 build() {
 	export BUILD_SDCC=1
 	export BUILD_SDCC_HTTP=1
