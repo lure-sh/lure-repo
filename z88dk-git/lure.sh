e28d271bcc6b4ab7f325bd0a3e31de4d2a686068
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-05-15 11:31:49 +0700

    Implement `version()`

--- z88dk-git/lure.sh
+++ z88dk-git/lure.sh
@@ -12,6 +12,11 @@ checksums=('SKIP')
 
 build_deps_ubuntu=('build-essential' 'bison' 'flex' 'libxml2-dev' 'subversion' 'zlib1g-dev' 'm4' 'ragel' 're2c' 'dos2unix' 'texinfo' 'texi2html' 'gdb' 'curl' 'perl' 'cpanminus' 'ccache' 'libboost-all-dev' 'libmodern-perl-perl' 'libyaml-perl' 'liblocal-lib-perl' 'libcapture-tiny-perl' 'libpath-tiny-perl' 'libtext-table-perl' 'libdata-hexdump-perl' 'libregexp-common-perl' 'libclone-perl' 'libfile-slurp-perl')
 
+version() {
+	cd "$srcdir/z88dk"
+	git-version
+}
+
 build() {
 	export BUILD_SDCC=1
 	export BUILD_SDCC_HTTP=1
