5738c71aeaf346acc9376a5dde20e7226b2ddc70
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-05-15 11:22:42 +0700

    Add package desription

--- z88dk-git/lure.sh
+++ z88dk-git/lure.sh
@@ -1,6 +1,7 @@
 name='z88dk-git'
 version='21023.a09ee3d7a'
 release=1
+desc='The development kit for Z80 computers'
 architectures=('amd64')
 
 sources=('git+https://github.com/z88dk/z88dk?~recursive=true')
