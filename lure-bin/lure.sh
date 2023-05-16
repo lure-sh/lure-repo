1105ac3b1e031a7114f81c686ceb1760ac015a42
Elara Musayelyan <elara@elara.ws>
2023-05-16 01:16:51 +0000

    upg(lure-bin): 0.0.8; fix(lure-bin): add conflicting packages

--- lure-bin/lure.sh
+++ lure-bin/lure.sh
@@ -1,5 +1,5 @@
 name='lure-bin'
-version=0.0.7
+version=0.0.8
 release=2
 desc='Linux User REpository'
 homepage='https://gitea.elara.ws/Elara6331/lure'
@@ -8,22 +8,22 @@ maintainer_ru='Элара Мусаелян <elara@elara.ws>'
 architectures=('amd64' 'arm64' 'riscv64' 'arm6' '386')
 license=('GPL-3.0-or-later')
 provides=('lure')
-conflicts=('lure')
+conflicts=('lure' 'linux-user-repository' 'linux-user-repository-bin' 'linux-user-repository-git')
 
-sources_arm64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.7/lure_0.0.7_linux_aarch64.tar.gz")
-checksums_arm64=('0808daf8d6f373642eabfa64375f8a69cb519c5d1c8208642a438eaca4c84b7a')
+sources_arm64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.8/lure_0.0.8_linux_aarch64.tar.gz")
+checksums_arm64=('b368c7ad715bd9dac539d9f74f98b947ae7f4e124a31d8716f42661a1488042f')
 
-sources_arm=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.7/lure_0.0.7_linux_armv6.tar.gz")
-checksums_arm=('b7b6a50e72658c671dbb7803445692939d6ef924d86687d461546d2836395418')
+sources_arm=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.8/lure_0.0.8_linux_armv6.tar.gz")
+checksums_arm=('fde1102eded9368f92d2320ebe41df95463b59f7b19bfb1f304959dcbd17bc81')
 
-sources_amd64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.7/lure_0.0.7_linux_x86_64.tar.gz")
-checksums_amd64=('b7723a18ae54c5fbf7c052e6b03fbe68bda5fbb9a0b0b668ef4ac74204788bfe')
+sources_amd64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.8/lure_0.0.8_linux_x86_64.tar.gz")
+checksums_amd64=('4354efafe87be9365fbee0b7de74420ebed87db6ffa030e5f4be9e8ebbadd8cf')
 
-sources_386=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.7/lure_0.0.7_linux_i386.tar.gz")
-checksums_386=('ea04d08dc4aa89bc97bfb6e65c6dad670853bf4d351f3f1619ef868cbad427c0')
+sources_386=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.8/lure_0.0.8_linux_i386.tar.gz")
+checksums_386=('de54752df82483ea0e86af678743822c4e4685d8a3e14eaf886366bfe3b30677')
 
-sources_riscv64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.7/lure_0.0.7_linux_riscv64.tar.gz")
-checksums_riscv64=('12c893256ba803b7bf9885d00441aedd0758495ccf2b8158a57d5b3a93cf6981')
+sources_riscv64=("https://gitea.elara.ws/Elara6331/lure/releases/download/v0.0.8/lure_0.0.8_linux_riscv64.tar.gz")
+checksums_riscv64=('e325669cf6524a1246ff1a2fbcaeecc5b1f3a600f00172d307a3de5974877f20')
 
 package() {
 	install-binary ./lure
