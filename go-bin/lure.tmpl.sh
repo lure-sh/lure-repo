b2119e239c0a35bff6429cb745c21fe2d166645b
Elara Musayelyan <elara@elara.ws>
2023-06-25 12:50:40 -0700

    updater: add go-bin template

---
+++ go-bin/lure.tmpl.sh
@@ -0,0 +1,30 @@
+name="go-bin"
+version=%s
+release=1
+desc="Compiler and tools for the Go programming language"
+desc_ru="Компилятор и инструменты для языка программирования Go"
+homepage="https://go.dev/"
+maintainer='Elara Musayelyan <elara@elara.ws>'
+maintainer_ru='Элара Мусаелян <elara@elara.ws>'
+architectures=('amd64' 'arm64' 'arm6' '386')
+license=('BSD-3-Clause')
+provides=('go' 'golang')
+conflicts=('go' 'golang' 'golang-bin' 'golang-go')
+
+sources_amd64=("https://dl.google.com/go/go${version}.linux-amd64.tar.gz")
+checksums_amd64=('%s')
+
+sources_arm64=("https://dl.google.com/go/go${version}.linux-arm64.tar.gz")
+checksums_arm64=('%s')
+
+sources_arm6=("https://dl.google.com/go/go${version}.linux-armv6l.tar.gz")
+checksums_arm6=('%s')
+
+sources_386=("https://dl.google.com/go/go${version}.linux-386.tar.gz")
+checksums_386=('%s')
+
+package() {
+	mkdir -p "$pkgdir/usr/lib/" "$pkgdir/usr/bin"
+	cp -r "$srcdir/go" "$pkgdir/usr/lib/go"
+	ln -s "$pkgdir/usr/lib/go/bin/go" "$pkgdir/usr/bin/go"
+}
