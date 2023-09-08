77d897436210b701a3f7d350a70cbdfb97fb7490
Elara Musayelyan <elara@elara.ws>
2023-09-08 21:50:52 +0000

    add: scdoc-git

---
+++ scdoc-git/lure.sh
@@ -0,0 +1,31 @@
+name='scdoc-git'
+version='148.afeda24'
+release='1'
+desc='scdoc is a simple man page generator for POSIX systems written in C99.'
+homepage='https://github.com/mvdan/sh'
+maintainer="Elara Musayelyan <elara@elara.ws>"
+maintainer_ru='Элара Мусаелян <elara@elara.ws>'
+architectures=('amd64' 'arm64' 'arm' '386' 'riscv64')
+license=('custom:None')
+provides=('scdoc')
+conflicts=('scdoc')
+
+build_deps=('gcc')
+
+sources=('git+https://git.sr.ht/~sircmpwn/scdoc')
+checksums=('SKIP')
+
+version() {
+	cd "$srcdir/scdoc"
+	git-version
+}
+
+build() {
+	cd "$srcdir/scdoc"
+	make PREFIX='/usr' DESTDIR="$pkgdir"
+}
+
+package() {
+	cd "$srcdir/scdoc"
+	make PREFIX='/usr' DESTDIR="$pkgdir" install
+}
