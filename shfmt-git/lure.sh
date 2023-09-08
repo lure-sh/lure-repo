90be8a9d58e8a1244ad25ebf6a247ecf28ca7d12
Elara Musayelyan <elara@elara.ws>
2023-09-08 20:48:36 +0000

    fix(shfmt-git): switch to the shfmt that LURE uses for consistency

--- shfmt-git/lure.sh
+++ shfmt-git/lure.sh
@@ -1,27 +1,33 @@
 name='shfmt-git'
-version='3241.08049c8'
+version='3570.6fe8d00'
 release='1'
-desc='A shell formatter. Supports POSIX Shell, Bash, and mksh.'
-homepage='https://github.com/patrickvane/shfmt'
-maintainer="Евгений Храмов <xpamych@yandex.ru>"
-architectures=('all')
+desc='Format shell programs'
+homepage='https://github.com/mvdan/sh'
+maintainer="Elara Musayelyan <elara@elara.ws>"
+maintainer_ru='Элара Мусаелян <elara@elara.ws>'
+architectures=('amd64' 'arm64' 'arm' '386' 'riscv64')
 license=('BSD-3-Clause license')
 provides=('shfmt')
 conflicts=('shtmt')
 
-sources=("git+https://github.com/patrickvane/shfmt.git")
+build_deps=('golang')
+build_deps_arch=('go')
+build_deps_alpine=('go')
+build_deps_opensuse=('go')
+
+sources=("git+https://github.com/mvdan/sh.git")
 checksums=('SKIP')
 
 version() {
-	cd "$srcdir/${provides}"
+	cd "$srcdir/sh"
 	git-version
 }
 
 build() {
-	cd "$srcdir/shfmt/cmd/shfmt"
-	go build
+	cd "$srcdir/sh"
+	go build ./cmd/shfmt
 }
 
 package() {
-	install -Dm755 "$srcdir/shfmt/cmd/shfmt/shfmt" "${pkgdir}/usr/bin/shfmt"
+	install-binary "$srcdir/sh/shfmt"
 }
