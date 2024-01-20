1223cfc745d5128aa4e8ac5713776ba4fc745b62
Elara <elara@elara.ws>
2024-01-19 22:08:39 -0800

    fix(scdoc-git): set license to MIT

--- scdoc-git/lure.sh
+++ scdoc-git/lure.sh
@@ -3,10 +3,10 @@ version='148.afeda24'
 release='1'
 desc='scdoc is a simple man page generator for POSIX systems written in C99.'
 homepage='https://git.sr.ht/~sircmpwn/scdoc'
-maintainer="Elara Musayelyan <elara@elara.ws>"
+maintainer="Elara M <elara@elara.ws>"
 maintainer_ru='Элара Мусаелян <elara@elara.ws>'
 architectures=('amd64' 'arm64' 'arm' '386' 'riscv64')
-license=('custom:None')
+license=('MIT')
 provides=('scdoc')
 conflicts=('scdoc')
 
